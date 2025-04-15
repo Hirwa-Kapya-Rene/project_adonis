6fb29357152109a7a01fdf15744218514d39b697
Hirwa Rene <hirwarene6@gmail.com>
2025-04-15 10:40:07 +0200

    created model short_urls

---
+++ app/models/short_url.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class ShortUrl extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare code: string
+
+  @column()
+  declare short_url: string
+
+  @column()
+  declare original_url: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1744706203147_create_short_urls_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'short_urls'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('code').unique()
+      table.string('short_url').notNullable()
+      table.string('original_url').notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
