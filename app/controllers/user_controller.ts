3ee1fba36d72d6309819ff4ab37debae6b2b312d
Hirwa Rene <hirwarene6@gmail.com>
2025-04-27 22:17:27 +0200

    added methods to user controller

---
+++ app/controllers/user_controller.ts
@@ -0,0 +1,56 @@
+import { HttpContext } from '@adonisjs/core/http'
+import {createUserValidator} from '#validators/user'
+import User from '#models/user'
+
+
+export default class UserController {
+
+  public async store({ view, request, response}:HttpContext) {
+      const payload = await request.validateUsing(createUserValidator)
+      
+      const user = await User.create({
+        full_name: payload.full_name,
+        email: payload.email,
+        password: payload.password
+      })
+      console.log(user);
+
+      return response.redirect('/list')
+    }
+
+
+  public async login({ request, response, auth, session }: HttpContext) {
+    try {
+      const { email, password } = request.only(['email', 'password'])
+
+      const user = await User.verifyCredentials(email, password)
+      
+      await auth.use('web').login(user)
+      session.flash('success', 'Login successful')
+      return response.redirect('/list')
+    } 
+    catch (error) {
+      session.flash('notification', {
+        type: 'error',
+        message: error.message || 'An error occurred. Please try again.',
+      })
+      return response.redirect().back()
+    }
+  }
+
+
+  public async showLoginForm({ view }: HttpContext) {
+    return view.render('pages/login')
+  }
+
+  public async showSigninForm({ view }: HttpContext) {
+    return view.render('pages/signin')
+  }
+
+  public async logout({ auth, response }: HttpContext) {
+    await auth.use('web').logout()
+    return response.redirect('/login') 
+  }
+  
+  
+}
